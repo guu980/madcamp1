c2907d2423329563d4678a3de941a95c5c62f7a4
ssoymikey <ssoymikey@kaist.ac.kr>
2019-12-28 00:12:57 +0900

    ssoy

--- GGALGOM_1/app/src/main/java/com/example/ggalgom_1/MainActivity.java
+++ GGALGOM_1/app/src/main/java/com/example/ggalgom_1/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ggalgom_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,5 +36,21 @@ public class MainActivity extends AppCompatActivity {
         ts3.setContent(R.id.content3);
         ts3.setIndicator("TAB 3");
         tabHost1.addTab(ts3);
+
+        final int img[] = {R.drawable.minieon01, R.drawable.minieon02, R.drawable.minieon03,
+                            R.drawable.minieon04, R.drawable.minieon05, R.drawable.minieon06,
+                R.drawable.minieon07, R.drawable.minieon08, R.drawable.minieon09, R.drawable.minieon10, R.drawable.minieon11,
+                R.drawable.minieon12, R.drawable.minieon13, R.drawable.minieon14, R.drawable.minieon15, R.drawable.minieon16,
+                R.drawable.minieon17, R.drawable.minieon18, R.drawable.minieon19, R.drawable.minieon20};
+
+        GalleryAdapter adapter = new GalleryAdapter(
+                getApplicationContext(),
+                R.layout.row,
+                img);
+
+        Gallery g = (Gallery)findViewById(R.id.gallery);
+        g.setAdapter(adapter);
+
+        final ImageView iv = (ImageView)findViewById(R.id.imageView1);
     }
 }
