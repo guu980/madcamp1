fac228b5b92e5f2cead4dd8b91ca8503d15fa652
LHK <guu980@gmailo.com>
2019-12-28 12:23:12 +0900

    tab12finished

---
+++ app/src/main/java/com/example/ggalgom_1/Contact.java
@@ -0,0 +1,22 @@
+package com.example.ggalgom_1;
+
+public class Contact {
+    private Long id;
+    private String name;
+    private String phonenumber;
+
+    public void setId(Long input_id)
+    {
+        this.id = input_id;
+    }
+
+    public void setPhoneNumber(String input_pn)
+    {
+        this.phonenumber = input_pn;
+    }
+
+    public void setName(String input_name)
+    {
+        this.name = input_name;
+    }
+}
---
+++ app/src/main/java/com/example/ggalgom_1/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.ggalgom_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup();
+
+        // 첫번째 탭
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("Tab 1");
+        tabHost1.addTab(ts1);
+
+        //두번째 탭
+        Context context = getApplicationContext();
+        ContactUtil contactUtil = new ContactUtil(context);
+        List<Contact> contacts = contactUtil.getContactList();
+
+
+        PhoneBook phoneBook = new PhoneBook();
+        // context = getApplicationContext();
+        List<PhoneBook> datas = phoneBook.getcontacts(this); // it might should changd this to context
+
+        RecyclerView recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //for debugging
+        TextView textView = (TextView)findViewById(R.id.showing);
+
+        RecyclerAdapter adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+
+        for(int i=datas.size()-1; i>=0; i--)
+        {
+            PhoneBook temp = datas.get(i);
+            adapter.addItem(temp);
+            textView.setText(temp.getName());
+        }
+        adapter.notifyDataSetChanged();
+
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("TAB 2");
+        tabHost1.addTab(ts2);
+
+        //세번째 탭
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
+        ts3.setContent(R.id.content3);
+        ts3.setIndicator("TAB 3");
+        tabHost1.addTab(ts3);
+
+        final int img[] = {R.drawable.minieon01, R.drawable.minieon02, R.drawable.minieon03,
+                R.drawable.minieon04, R.drawable.minieon05, R.drawable.minieon06,
+                R.drawable.minieon07, R.drawable.minieon08, R.drawable.minieon09, R.drawable.minieon10, R.drawable.minieon11,
+                R.drawable.minieon12, R.drawable.minieon13, R.drawable.minieon14, R.drawable.minieon15, R.drawable.minieon16,
+                R.drawable.minieon17, R.drawable.minieon18, R.drawable.minieon19, R.drawable.minieon20};
+
+        GalleryAdapter grid_adapter = new GalleryAdapter(
+                getApplicationContext(),
+                R.layout.row,
+                img);
+
+        Gallery g = (Gallery)findViewById(R.id.gallery);
+        g.setAdapter(grid_adapter);
+
+        final ImageView iv = (ImageView)findViewById(R.id.imageView1);
+    }
+}
